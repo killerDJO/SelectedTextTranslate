d5df30c50e1be4fc31aa183130dfb1ec5a63b409
killerDJO <killerDJO@gmail.com>
2014-01-04 18:41:32 +0200

    files regrouping

---
+++ BuildScripts/PostBuildCopyDll.bat
@@ -0,0 +1,37 @@
+ECHO OFF
+ECHO "Copy DLLs..."
+
+SET OutputDir=%1
+SET ProjectDir=%2
+
+REM Validate output folder
+IF "%OutputDir%"=="" GOTO NO_OUTPUT_SPECIFIED
+IF NOT EXIST "%OutputDir%" GOTO OUTPUT_INVALID
+
+REM Validate project folder
+IF "%ProjectDir%"=="" GOTO NO_PROJECT_SPECIFIED
+IF NOT EXIST "%ProjectDir%" GOTO PROJECT_INVALID
+
+XCOPY /y "%ProjectDir%libcurl\*.dll" %OutputDir%
+XCOPY /y "%ProjectDir%jsoncpp\build\vs71\release\lib_json\*.dll" %OutputDir%
+XCOPY /y "%ProjectDir%Bin\*.dll" %OutputDir%
+
+GOTO END
+
+:NO_OUTPUT_SPECIFIED
+	ECHO No output directory specified.
+	GOTO END
+
+:OUTPUT_INVALID
+	ECHO The output directory specified is invalid.  Output must be an existing, fully qualified folder.
+	GOTO END
+
+:NO_PROJECT_SPECIFIED
+	ECHO No output directory specified.
+	GOTO END
+
+:PROJECT_INVALID
+	ECHO The output directory specified is invalid.  Output must be an existing, fully qualified folder.
+	GOTO END
+
+:END
